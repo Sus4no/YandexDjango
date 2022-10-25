d3dba6d15ff7b56281ea52427d5aa1d239ca3566
Sus4no <96766854+Sus4no@users.noreply.github.com>
2022-10-25 16:39:41 +0300

    Add files via upload

--- YandexDjango/catalog/tests.py
+++ YandexDjango/catalog/tests.py
@@ -2,12 +2,19 @@ from django.test import TestCase, Client
 
 
 class StaticUrlTests(TestCase):
+    args = {
+        '': 200,
+        '123/': 200,
+        '65/': 200,
+        'abc/': 404,
+        'a.s71/': 404,
+        '-149/': 404,
+        '-pqoi/': 404,
+        '12.751/': 404,
+        '.112/': 404,
+    }
+
     def test_catalog_endpoint(self):
-        response = Client().get('/catalog/')
-        self.assertEqual(response.status_code, 200)
-
-        response = Client().get('/catalog/123/')
-        self.assertEqual(response.status_code, 200)
-
-        response = Client().get('catalog/abc/')
-        self.assertEqual(response.status_code, 404)
+        for i in self.args.keys():
+            response = Client().get('/catalog/{}'.format(i))
+            self.assertEqual(response.status_code, self.args[i])
--- YandexDjango/homepage/tests.py
+++ YandexDjango/homepage/tests.py
@@ -2,6 +2,6 @@ from django.test import TestCase, Client
 
 
 class StaticUrlTests(TestCase):
-    def test_catalog_endpoint(self):
+    def test_homepage_endpoint(self):
         response = Client().get('/')
         self.assertEqual(response.status_code, 200)
