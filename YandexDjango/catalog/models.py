a58d53d8f2625c74d9df371d372820c7a0b1165f
Sus4n00 <96766854+Sus4no@users.noreply.github.com>
2022-11-02 13:22:43 +0300

    Add files via upload

--- YandexDjango/catalog/models.py
+++ YandexDjango/catalog/models.py
@@ -29,7 +29,7 @@ class Category(Core):
 class Item(Core):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
-    text = models.TextField(validators=[validate_if_words_there, ],
+    text = models.TextField(validators=[validate_if_words_there],
                             help_text='Описание должно быть больше 2х слов' +
                                       ' и содержать слова ' +
                                       '\"превосходно, роскошно\"')
